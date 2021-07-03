9e17ed904ef12f848d1eec0dc29fc61bf3d584e6
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2021-07-03 11:40:53 +0000

    chore: upgrade dependencies

--- src/http-client.ts
+++ src/http-client.ts
@@ -3,8 +3,7 @@ import { fetch } from 'extra-fetch'
 import { post } from 'extra-request'
 import { ok, toJSON } from 'extra-response'
 import { json, url, signal, keepalive, basicAuth } from 'extra-request/lib/es2018/transformers'
-import { JsonRpcResponse } from '@blackglory/types'
-import { Json } from 'justypes'
+import { JsonRpcResponse, Json } from 'justypes'
 import { timeoutSignal } from 'extra-promise'
 
 export interface IClientOptions {
