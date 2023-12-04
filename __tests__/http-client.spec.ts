b8cd4eece06ecd2761c6decb7c1d78e617bad0cd
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2023-12-04 02:41:50 +0000

    test: add cases

--- __tests__/http-client.spec.ts
+++ __tests__/http-client.spec.ts
@@ -1,6 +1,8 @@
 import { buildServer } from './http-client.mock'
 import { createClient } from '@src/http-client'
 import { startService, stopService, getAddress } from './utils'
+import { createBatchClient } from '@src/http-client.js'
+import { createBatchProxy } from 'delight-rpc'
 
 beforeAll(() => startService(buildServer))
 afterAll(stopService)
@@ -18,5 +20,17 @@ describe('createClient', () => {
     const result = await client.echo('hello')
 
     expect(result).toStrictEqual('hello')
+  })
+
+  test('echo (batch)', async () => {
+    const client = createBatchClient({
+      server: getAddress()
+    })
+    const proxy = createBatchProxy<IAPI>()
+
+    const result = await client.parallel(proxy.echo('hello'))
+
+    expect(result.length).toBe(1)
+    expect(result[0].unwrap()).toBe('hello')
   })
 })
